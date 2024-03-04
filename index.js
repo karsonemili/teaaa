6f69c37edcf7b9b2734db5716090ee370e313216
karsonemili <80674829+karsonemili@users.noreply.github.com>
2024-03-04 17:28:38 +0000

    added helper fun-n

---
+++ index.js
@@ -0,0 +1,9 @@
+function calculateRectangleArea(length, width) {
+    return length * width;
+  }
+  
+  // Example of usage:
+  const rectangleLength = 5;
+  const rectangleWidth = 10;
+  const area = calculateRectangleArea(rectangleLength, rectangleWidth);
+  console.log(`The area of the rectangle is: ${area}`);
